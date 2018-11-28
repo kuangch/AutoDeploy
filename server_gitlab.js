7eb8f64c7fe89762653335a4dcd82b9701072754
kuangch <kuangch_8017@qq.com>
2018-11-28 15:16:40 +0800

    优化脚本执行提示

--- server_gitlab.js
+++ server_gitlab.js
@@ -4,6 +4,7 @@
 let express = require('express');
 let process = require('child_process');
 let bodyParser = require('body-parser');
+let colors = require('colors');
 let app = express();
 let config = require('./gitlab_hook_config')
 
@@ -11,25 +12,27 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 function exeScript(autoScript, callback) {
-    console.error('chmod autoScript...');
+    console.error('chmod autoScript...'.grey);
     process.exec('chmod a+x ' + autoScript,
         function (error, stdout, stderr) {
             if (error !== null) {
                 console.error(`chmod autoScript failed\n${stderr}`);
                 callback(`<pre>chmod fail!!!\n ${stdout}\n${error}</pre>`);
             } else {
-                console.log('chmod autoScript success');
-                console.log('exec autoScript...');
-                process.exec(autoScript,
-                    function (error, stdout, stderr) {
-                        console.log('exec script stdout ========================\n' + stdout);
-                        console.log('exec script stderr ========================\n' + stderr);
-                        if (error !== null) {
-                            callback(`<pre>fail!!!\n${error}</pre>`);
-                        } else {
-                            callback(`<pre>done!!!\n${stdout}</pre>`);
-                        }
-                    });
+                console.log('chmod autoScript success'.green);
+                console.log(`exec script.. `.grey);
+                let script = process.spawn(autoScript);
+                script.stdout.on('data',(data)=>{
+                    console.log(`${data}`);
+                })
+                script.stderr.on('data',(data)=>{
+                    console.log(`${data}`.red);
+                })
+                script.on('close',(code)=>{
+                    console.log(`${new Date().toLocaleString()} script exited with code ${code}`.green);
+
+                })
+                callback(`<pre>success</pre>`);
             }
         });
 }
@@ -41,7 +44,8 @@ app.post('/webhook', function (req, res) {
 
     let projectName = req.body.project.name;
 
-    console.log(`HOOK: project: ${projectName}，action: ${action}, token: ${token}`);
+    console.log(`${new Date().toLocaleString()} > hook start..  `.inverse);
+    console.log(`project: ${projectName.blue}, action: ${action.green}, token: ${token.yellow}`);
 
     if (!!action && action.toLowerCase() === 'push hook') {
 
@@ -58,11 +62,12 @@ app.post('/webhook', function (req, res) {
         }
 
         exeScript(script,function (result) {
+            console.log(`hook end  `.grey);
             res.send(result)
         })
 
     } else {
-        console.log('忽略: 不是 push 操作 ');
+        console.log('忽略: 不是 push 操作 '.grey);
         res.send('<pre>不是 push 操作？</pre>');
     }
 });
